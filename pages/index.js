38b13fa38f0d9a9677f352679b815ee224760529
NipunNVemula06 <nipunvemula4@gmail.com>
2021-12-25 11:48:19 +0530

    added api request from tmdb

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Header from '../components/Header';
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>Hulu 2.0</title>
-			</Head>
+  return (
+    <div>
+      <Head>
+        <title>Hulu 2.0</title>
+      </Head>
 
-			{/* header */}
-			<Header />
-			{/* Navbar */}
-			{/* Results */}
-		</div>
-	);
+      {/* header */}
+      <Header />
+      {/* Navbar */}
+      <Navbar />
+      {/* Results */}
+    </div>
+  );
 }
